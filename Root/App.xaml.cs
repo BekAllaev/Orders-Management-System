eaef21cd820ac5079e9b745cfde5d55a1aba1143
Bekzod Allaev <Allaev_@outlook.com>
2019-10-22 22:09:10 +0500

    Add project files.

---
+++ Root/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Prism;
+using System.Windows;
+
+namespace Root
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Root/Bootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Root
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+
+    }
+}
