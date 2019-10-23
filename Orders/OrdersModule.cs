0a50b1d6179eac02e90d8fe5f0ac4fd6b7a6597a
Bekzod Allaev <Allaev_@outlook.com>
2019-10-23 11:16:33 +0500

    Added central class of Orders module.

---
+++ Orders/OrdersModule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using Orders.Views;
+
+namespace Orders
+{
+    public class OrdersModule : IModule
+    {
+        IRegionManager regionManager;
+
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            regionManager = containerProvider.Resolve<IRegionManager>();
+
+            regionManager.RequestNavigate("OrdersRegion", "OrdersDashBoardView");
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<OrdersDashBoardView>();
+        }
+    }
+}
