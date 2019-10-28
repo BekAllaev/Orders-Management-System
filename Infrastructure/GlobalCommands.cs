193e797ee74a33363684deafda25b4f230c64e5b
Bekzod Allaev <Allaev_@outlook.com>
2019-10-28 23:40:33 +0500

    Navigation into Orders module is implemented

---
+++ Infrastructure/GlobalCommands.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Commands;
+
+namespace Infrastructure
+{
+    public static class GlobalCommands
+    {
+        /// <summary>
+        /// Command which executes navigation into module from which button Manage was pressed
+        /// </summary>
+        public static CompositeCommand NavigateToCompositeCommand = new CompositeCommand();
+    }
+}
--- Root/ViewModels/ContentViewViewModel.cs
+++ Root/ViewModels/ContentViewViewModel.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿using Microsoft.Practices.Unity;
+using Prism.Mvvm;
+using Prism.Regions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Infrastructure;
+using Prism.Commands;
 
 namespace Root.ViewModels
 {
-    class ContentViewViewModel
+    public class ContentViewModel : BindableBase
     {
+        IRegionManager regionManager;
+
+        public ContentViewModel(IRegionManager regionManager)
+        {
+            this.regionManager = regionManager;
+
+            NavigateToCommand = new DelegateCommand<string>(NavigateToExecute);
+
+            GlobalCommands.NavigateToCompositeCommand.RegisterCommand(NavigateToCommand);
+        }
+
+        public DelegateCommand<string> NavigateToCommand { set; get; }
+
+        private void NavigateToExecute(string targetView)
+        {
+            regionManager.RequestNavigate("ContentRegion", targetView);
+        }
     }
 }
