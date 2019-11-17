928af4a88673fc2c19c807a86d7d054a302d1864
Bekzod Allaev <Allaev_@outlook.com>
2019-11-18 00:42:44 +0500

    Settings service was added to Infrastructure class library

---
+++ Infrastructure/Settings/UserSettingsRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Settings
+{
+    public class UserSettingsRepository : IUserSettingsRepository
+    {
+        private Properties.Settings Default = Properties.Settings.Default; //Instance of the user settings. ???I think there is better comment for this field???
+
+        public object ReadSetting(string settingName)
+        {
+            return Default.Properties[settingName];
+        }
+
+        public void WriteSetting(string settingName, object value)
+        {
+            Default.OrdersMainView = (string)value;
+
+            Default.Save();
+        }
+    }
+}
