c4b05a4377b20d4993cbaffa4a3d99bd07416eeb
Bekzod Allaev <Allaev_@outlook.com>
2019-12-15 00:51:42 +0500

    Settings module has been added.

--- Settings/Properties/AssemblyInfo.cs
+++ Settings/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5bca5069-4d86-403c-8dd9-035848b64dfb")]
+[assembly: Guid("73df493f-268e-4d66-a8a9-203e9b93e0b6")]
 
 // Version information for an assembly consists of the following four values:
 //
