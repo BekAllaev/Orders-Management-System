2aa8166ad9e3fd7b1baac18da913c8098b0eac77
Bekzod Allaev <Allaev_@outlook.com>
2020-01-12 15:42:10 +0500

    ReactiveUI and INavigationAware were included

--- Dashboard/ViewModels/EmployeeStatisticViewModel.cs
+++ Dashboard/ViewModels/EmployeeStatisticViewModel.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ReactiveUI;
+using Prism.Regions;
 
 namespace Dashboard.ViewModels
 {
-    class EmployeeStatisticViewModel
+    public class EmployeeStatisticViewModel : ReactiveObject, INavigationAware
     {
+        public bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
