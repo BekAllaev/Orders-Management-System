ea39644f529cc0781c61ebe27d639f872049e34c
Bekzod Allaev <Allaev_@outlook.com>
2020-05-08 00:20:52 +0500

    Invoice ViewModel was created

---
+++ Orders/ViewModels/InvoiceViewModels.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLocal;
+using Orders.Events;
+using Prism.Regions;
+using ReactiveUI;
+
+namespace Orders.ViewModels
+{
+    public class InvoiceViewModel : ReactiveObject, INavigationAware
+    {
+        NorthwindContext northwindContext;
+
+        public InvoiceViewModel(NorthwindContext northwindContext)
+        {
+            this.northwindContext = northwindContext;
+
+            MessageBus.Current.Listen<NewOrderCreated>().
+                Subscribe(orderCreatedEvent => OnNewOrderCreated(orderCreatedEvent.OrderId));
+        }
+
+        public ObservableCollection<OrderObject> Orders { get; set; }
+        public ObservableCollection<OrderDetailObject> OrderDetails { get; set; }
+
+        #region INavigationAware interface implementation
+        public bool IsNavigationTarget(NavigationContext navigationContext) { return true; }
+
+        public void OnNavigatedFrom(NavigationContext navigationContext) { }
+
+        public void OnNavigatedTo(NavigationContext navigationContext) { }
+        #endregion
+
+        private void OnNewOrderCreated(int id)
+        {
+            var orders = northwindContext.Orders.Where(o => o.OrderID == id).ToList();
+            var orderDetails = northwindContext.Order_Details.Where(o => o.OrderID == id).ToList();
+
+            Orders = new ObservableCollection<OrderObject>
+                (orders.Select(o => new OrderObject()
+                {
+                    OrderId = o.OrderID,
+                    OrderDate = o.OrderDate.Value,
+                    EmployeeName = o.Employee.FirstName + " " + o.Employee.LastName,
+                    CustomerName = o.Customer.CompanyName
+                }
+                ));
+
+            OrderDetails = new ObservableCollection<OrderDetailObject>
+                (orderDetails.Select(o => new OrderDetailObject()
+                {
+                    OrderId = o.OrderID,
+                    ProductName = o.Product.ProductName,
+                    UnitPrice = o.UnitPrice,
+                    Quantity = o.Quantity,
+                    Discount = o.Discount,
+                    SubTotal = (decimal)((float)o.UnitPrice * o.Quantity * (1 - o.Discount))
+                }
+                ));
+
+            MessageBus.Current.SendMessage<OrerDataCreated>(new OrerDataCreated());
+        }
+
+        #region Screen objects
+        public class OrderObject
+        {
+            public int OrderId { get; set; }
+            public string CustomerName { get; set; }
+            public string EmployeeName { get; set; }
+            public DateTime OrderDate { get; set; }
+        }
+
+        public class OrderDetailObject
+        {
+            public int OrderId { get; set; }
+            public string ProductName { get; set; }
+            public decimal UnitPrice { get; set; }
+            public short Quantity { get; set; }
+            public float Discount { get; set; }
+            public decimal SubTotal { get; set; }
+        }
+        #endregion
+    }
+}
--- Orders/Views/InvoiceView.xaml.cs
+++ Orders/Views/InvoiceView.xaml.cs
@@ -11,7 +11,13 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.IO;
+using System.Diagnostics;
+using System.Reactive.Linq;
+using System.Reactive;
+using ReactiveUI;
+using Orders.ViewModels;
+using Orders.Events;
 
 namespace Orders.Views
 {
@@ -23,6 +29,31 @@ namespace Orders.Views
         public InvoiceView()
         {
             InitializeComponent();
+
+            MessageBus.Current.Listen<OrerDataCreated>()
+                .Subscribe(orderCreatedEvent => SetDataSources());
+        }
+
+        private void SetDataSources()
+        {
+            this.InvoiceReport.DataSources.Clear();
+
+            var orders = (this.DataContext as InvoiceViewModel).Orders;
+            var orderDetails = (this.DataContext as InvoiceViewModel).OrderDetails;
+
+            this.InvoiceReport.DataSources.Add(new Syncfusion.Windows.Reports.ReportDataSource()
+            {
+                Name = "Orders",
+                Value = orders
+            });
+
+            this.InvoiceReport.DataSources.Add(new Syncfusion.Windows.Reports.ReportDataSource()
+            {
+                Name = "OrderDetails",
+                Value = orderDetails
+            });
+
+            this.InvoiceReport.RefreshReport();
         }
     }
 }
