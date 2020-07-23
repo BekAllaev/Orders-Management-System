a98780bb77e106640f3e99493cf376cb3d3e7d1f
Bekzod Allaev <Allaev_@outlook.com>
2020-07-23 22:24:35 +0500

    Model entites was removed to class library

---
+++ OMS.Data/Models/Invoice.cs
@@ -0,0 +1,24 @@
+namespace DataAccessLocal
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Invoice")]
+    public partial class Invoice
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Invoice()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int InvoiceId { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
