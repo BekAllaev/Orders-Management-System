c34cad4aa29c458334e95e22cf3ef0f74015ad3b
Bekzod Allaev <Allaev_@outlook.com>
2020-08-13 21:16:03 +0500

    Statistic Service was added

---
+++ OMS.WPFClient/Infrastructure/Services/StatisticService/IStatisticService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMS.WPFClient.Infrastructure.Services.StatisticService
+{
+    interface IStatisticService
+    {
+        public Task GetOrdersByCountries();
+    }
+}
